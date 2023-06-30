43285595e2e28de9452ae631c5246f732b01739f
BillyQuispe <carlos.quispe.v@tecsup.edu.pe>
2023-06-29 20:22:34 -0500

    Plan api

--- app/Http/Controllers/Api/PlanController.php
+++ app/Http/Controllers/Api/PlanController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Plan;
-use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 
 
 class PlanController extends Controller
@@ -14,9 +14,10 @@ class PlanController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        //
-    }
+{
+    $planes = Plan::all();
+    return response()->json(['data' => $planes], JsonResponse::HTTP_OK);
+}
 
     /**
      * Show the form for creating a new resource.
@@ -31,31 +32,36 @@ class PlanController extends Controller
      */
     public function store(Request $request)
     {
-        // Validar los datos de entrada
+        // Validar los campos requeridos
         $request->validate([
             'nombre' => 'required',
             'precio' => 'required',
             'sessions' => 'required',
         ]);
-
+    
+        // Crear un nuevo plan
         $plan = new Plan();
-        $plan->nombre = $request->input('nombre');
-        $plan->precio = $request->input('precio');
-        $plan->sessions = $request->input('sessions');
+        $plan->nombre = $request->nombre;
+        $plan->precio = $request->precio;
+        $plan->sessions = $request->sessions;
         $plan->save();
-
-        // Retornar una respuesta exitosa
-        return response($plan,Response::HTTP_CREATED)->json(['message' => 'Plan creado exitosamente']);
+    
+        // Devolver una respuesta JSON con el mensaje exitoso
+        return response()->json(['message' => 'Plan creado exitosamente'], JsonResponse::HTTP_CREATED);
     }
 
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
+    public function show($id)
+{
+    $plan = Plan::find($id);
+    if (!$plan) {
+        return response()->json(['message' => 'Plan no encontrado'], JsonResponse::HTTP_NOT_FOUND);
     }
+    return response()->json(['data' => $plan], JsonResponse::HTTP_OK);
+}
 
     /**
      * Show the form for editing the specified resource.
@@ -68,16 +74,40 @@ class PlanController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(Request $request, $id)
+{
+    $plan = Plan::find($id);
+    if (!$plan) {
+        return response()->json(['message' => 'Plan no encontrado'], JsonResponse::HTTP_NOT_FOUND);
     }
+
+    // Validar los campos requeridos
+    $request->validate([
+        'nombre' => 'required',
+        'precio' => 'required',
+        'sessions' => 'required',
+    ]);
+
+    $plan->nombre = $request->nombre;
+    $plan->precio = $request->precio;
+    $plan->sessions = $request->sessions;
+    $plan->save();
+
+    return response()->json(['message' => 'Plan actualizado exitosamente'], JsonResponse::HTTP_OK);
+}
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
+    public function destroy($id)
+{
+    $plan = Plan::find($id);
+    if (!$plan) {
+        return response()->json(['message' => 'Plan no encontrado'], JsonResponse::HTTP_NOT_FOUND);
     }
+
+    $plan->delete();
+
+    return response()->json(['message' => 'Plan eliminado exitosamente'], JsonResponse::HTTP_OK);
+}
 }
